9d6c55babbe9c1be596d506e897faaa52b224c91
Beatriz Guedes <beatrizguedessilva03@gmail.com>
2024-01-28 21:04:27 -0400

    ainda to tentnaod

--- TelaInicial.py
+++ TelaInicial.py
@@ -1,32 +1,42 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel
-from PyQt6.QtCore import Qt, QSize
+import wx
 
-def main():
-    app = QApplication(sys.argv)
-    widget = QWidget()
-    widget.setMinimumSize(QSize(500, 500))
-    widget.setMaximumSize(QSize(500, 500))
-    layout = QGridLayout(widget)
-    
-    # Ajuste o espaçamento entre os itens do layout
-    layout.setSpacing(10)  # Ajuste o valor conforme necessário
+class MyFrame(wx.Frame):
 
-    label = QLabel("Olá, mundo!")
-    label.setStyleSheet("font-size: 18px; font-family: 'Arial'; color: blue;")
-    label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-    layout.addWidget(label, 0, 0, 1, 2)  # Posiciona o QLabel na linha 0, coluna 0, com uma ocupação de 1 linha e 2 colunas
+    def __init__(self, parent, title):
+        super().__init__(parent, title=title, size=(500, 500))
 
-    button = QPushButton("Botão")
-    button.setMinimumSize(QSize(50, 30))
-    layout.addWidget(button, 1, 0, 1, 1)  # Posiciona o QPushButton na linha 1, coluna 0, com uma ocupação de 1 linha e 1 coluna
+        # Cria um painel
+        self.panel = wx.Panel(self)
 
-    otobutton = QPushButton("Outro Botão")
-    otobutton.setMinimumSize(QSize(50, 30))
-    layout.addWidget(otobutton, 1, 1, 1, 1)  # Posiciona o QPushButton na linha 1, coluna 1, com uma ocupação de 1 linha e 1 coluna
+        # Carrega a imagem
+        self.image = wx.Image("imagens\TI.png")
 
-    widget.show()
-    sys.exit(app.exec())
+        # Cria um bitmap da imagem
+        self.bitmap = wx.Bitmap(self.image)
 
-if __name__ == "__main__":
-    main()
+        # Cria um fundo com a imagem
+        self.background = wx.StaticBitmap(self.panel, bitmap=self.bitmap)
+
+        # Cria um botão
+        self.btn = wx.Button(self.panel, label="Clique aqui")
+        self.btn.Bind(wx.EVT_BUTTON, self.on_click)
+
+        # Cria um texto
+        self.txt = wx.StaticText(self.panel, label="Texto")
+
+        # Organiza os elementos na tela
+        self.btn.SetPosition((250, 200))
+        self.background.SetPosition((0, 0))
+        self.txt.SetPosition((250, 100))
+
+        self.btn.Raise()
+
+        self.Centre()
+
+    def on_click(self, event):
+        print("O botão foi clicado!")
+
+app = wx.App()
+frame = MyFrame(None, title="Tela com botão")
+frame.Show()
+app.MainLoop()
